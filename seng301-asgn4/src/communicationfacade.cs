98df22d05353d9cc1c0c49326b027e94def5e3fb
matthew.kanderka <matthew.kanderka@ucalgary.ca>
2017-04-04 12:54:00 -0600

    hy

--- seng301-asgn4/src/communicationfacade.cs
+++ seng301-asgn4/src/communicationfacade.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Frontend4.Hardware;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,19 @@ namespace seng301_asgn4.src
 {
     class CommunicationFacade
     {
+        private HardwareFacade hardwareFacade;
+        private BusinessRule businessRule;
+
+
+        public CommunicationFacade(HardwareFacade hardwareFacade, BusinessRule businessRule)
+        {
+            this.hardwareFacade = hardwareFacade;
+            this.businessRule = businessRule;
+        }
+
+        public void selectionButton(int button)
+        {
+            hardwareFacade.SelectionButtons[button].Press();
+        }
     }
 }
