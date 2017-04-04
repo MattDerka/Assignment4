1f510965c3e21010e81f4fbf8aefd8d5ca90433f
Matthew Kanderka <matthew.kanderka@ucalgary.ca>
2017-04-03 22:00:32 -0600

    product facade maybe done?

--- seng301-asgn4/src/productfacade.cs
+++ seng301-asgn4/src/productfacade.cs
@@ -1,12 +1,38 @@
-﻿using System;
+﻿using Frontend4.Hardware;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace seng301_asgn4.src
+class ProductFacade
 {
-    class ProductFacade
+    private HardwareFacade hardware;
+    public event EventHandler<ProductEventArgs> released;
+
+    public ProductFacade(HardwareFacade hardwarefacade)
     {
+        hardware = hardwarefacade;
+        foreach(var rack in hardware.ProductRacks)
+        {
+            rack.ProductRemoved += Rack_ProductRemoved;
+        }
+    }
+
+    public void despenseProduct(int rackNum)
+    {
+        var num = hardware.ProductRacks[rackNum].Count;
+        if (num == 0)
+        {
+            throw new Exception("No products left in rack");
+        }
+        hardware.ProductRacks[rackNum].DispenseProduct();
+    }
+
+
+    private void Rack_ProductRemoved(object sender, ProductEventArgs e)
+    {
+        this.released(sender, e);
     }
 }
+
